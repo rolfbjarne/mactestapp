2251988eeb75c6f7a8c0e9dfc1694f964bc90a57
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-11-05 16:57:20 +0100

    workaround

--- mactestapp/ViewController.cs
+++ mactestapp/ViewController.cs
@@ -8,18 +8,15 @@ namespace mactestapp {
 		public ViewController (IntPtr handle) : base (handle)
 		{
 		}
-
+		SavePanelDelegate del = new SavePanelDelegate ();
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 
 			// Do any additional setup after loading the view.
+			
 			var panel = new NSSavePanel ();
-			panel.DidChangeToDirectory += (o, args) => {
-				var url = args.NewDirectoryUrl;
-				Console.WriteLine (url?.Path);
-			};
-
+			panel.Delegate = del;
 			View.AddSubview (NSButton.CreateButton ("a", () => panel.RunModal ()));
 		}
 
@@ -31,6 +28,16 @@ namespace mactestapp {
 				base.RepresentedObject = value;
 				// Update the view, if already loaded.
 			}
+		}
+	}
+
+	class SavePanelDelegate : NSObject, INSOpenSavePanelDelegate {
+		[Export ("panel:directoryDidChange:")]
+		void DidChangeToDirectory (NSPanel sender, NSObject args)
+		{
+			Console.WriteLine ($"DidChangeDirectory ({sender}, {args} = {args?.GetType ()})");
+			if (args is NSUrl url)
+				Console.WriteLine ($"    url: {url?.Path}");
 		}
 	}
 }
