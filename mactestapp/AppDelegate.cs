c8ce6f0676f2b002546e26ad01e7c9c289461841
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-09-14 10:21:02 +0200

    wip

--- mactestapp/AppDelegate.cs
+++ mactestapp/AppDelegate.cs
@@ -7,28 +7,12 @@ using Foundation;
 namespace mactestapp {
 	[Register ("AppDelegate")]
 	public class AppDelegate : NSApplicationDelegate {
-		enum FooEnum {
-			Foo = 1,
-			Bar = 2
-		}
-
 		public AppDelegate ()
 		{
 		}
 
 		public override void DidFinishLaunching (NSNotification notification)
 		{
-			// Insert code here to initialize your application
-			var allInterfaces = NetworkInterface.GetAllNetworkInterfaces ();
-			Console.WriteLine ("Found {0} network interfaces", allInterfaces.Length);
-			foreach (var iface in allInterfaces)
-				Console.WriteLine ("    {0}", iface.Name);
-		}
-
-		public override void WillTerminate (NSNotification notification)
-		{
-			// Insert code here to tear down your application
 		}
 	}
 }
-
--- mactestapp/ViewController.cs
+++ mactestapp/ViewController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
 
 using AppKit;
 using Foundation;
@@ -13,6 +16,23 @@ namespace mactestapp {
 		{
 			base.ViewDidLoad ();
 
+			// Insert code here to initialize your application
+			var allInterfaces = NetworkInterface.GetAllNetworkInterfaces ();
+			var sb = new StringBuilder ();
+			sb.AppendFormat ("Found {0} network interfaces\n", allInterfaces.Length);
+			foreach (var iface in allInterfaces)
+				sb.AppendFormat ("    {0}\n", iface.Name);
+			Console.WriteLine (sb);
+
+			var text = new NSTextView (View.Bounds);
+			text.Value = sb.ToString ();
+			View.AddSubview (text);
+
+			new Thread (() => {
+				Thread.Sleep (5000);
+				Environment.Exit (40);
+			}).Start ();
 		}
 	}
 }
+
